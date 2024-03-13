a8f224d7c4586d55b3358d590713bf1c8ea85d01
GrumpyArdias <this.mario@outlook.com>
2024-03-13 09:09:34 +0100

    middleware has been removed, we use a filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ToDoModule } from './to-do/to-do.module';
 import { AuthModule } from './auth/auth.module';
-import { ErrorMiddleware } from './middlewares/error.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 @Module({
@@ -26,8 +25,4 @@ import { join } from 'path';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(ErrorMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
